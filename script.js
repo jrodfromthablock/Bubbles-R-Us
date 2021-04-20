2ae683c8f873d5c947a701b12b2fb9841cf9f6d3
Jennie Rodriguez <jennie@jennie-rodriguez.com>
2021-04-19 19:48:24 -0700

    changed the while loop to a simpler for loop

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44
 
 let output;
 
-var i = 0;
+// var i = 0;
 
-while (i < scores.length) {
+  // while (i < scores.length) {
+  //   output = "Bubble solution #" + i + " score: " + scores[i];
+  //   console.log(output);
+  //   i = i + 1;
+  // }
+
+for (var i = 0; i < scores.length; i = i + 1) {
   output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
-  i = i + 1;
 }
