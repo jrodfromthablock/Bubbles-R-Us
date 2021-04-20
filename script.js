4c8f598d6633a30d58c96a2b0eb6cf7ed766b891
Jennie Rodriguez <jennie@jennie-rodriguez.com>
2021-04-19 20:44:57 -0700

    added a for loop to catch the best scores stored within an empty array.  Then after the code executes...output the bestSolutions to the console

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ for (var i = 0; i < scores.length; i++) {
 
 console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + highScore);
+
+let bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+  if (scores[i] == highScore) {
+    bestSolutions.push(i);
+  }
+}
+
+console.log ("Solutions with the highest score: " + bestSolutions);
