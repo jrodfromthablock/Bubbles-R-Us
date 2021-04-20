214be211fca39118d534f9498cd2d168e45a5160
Jennie Rodriguez <jennie@jennie-rodriguez.com>
2021-04-19 20:14:13 -0700

    fixed post increment syntax

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let output;
   //   i = i + 1;
   // }
 
-for (var i = 0; i < scores.length; i = i++) {
-  output = "Bubble solution #" + i + " score: " + scores[i];
+for (var i = 0; i < scores.length; i++) {
+    output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
 }
