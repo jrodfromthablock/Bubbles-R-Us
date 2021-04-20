ed16b1600ca2a5be6b96f624723e9c7c4145cddb
Jennie Rodriguez <jennie@jennie-rodriguez.com>
2021-04-19 20:28:07 -0700

    added code to find the highest score of the bubble solutions array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 64, 66, 55, 51, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
+let highScore = 0;
 let output;
 
 // var i = 0;
@@ -13,4 +14,10 @@ let output;
 for (var i = 0; i < scores.length; i++) {
     output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
+  if (scores[i] > highScore) {
+    highScore = scores[i];
+  }
 }
+
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
